5dd9a347599e06ffd75d56726f6be85d39cc763a
Ori <ori@stallae.com>
2025-02-21 11:38:40 -0300

    feat: aff Header

--- src/screens/FormCalculator.jsx
+++ src/screens/FormCalculator.jsx
@@ -1,9 +1,11 @@
 import Calculator from "../sections/FormCalculator/Calculator.jsx";
 import Advantages from "../sections/FormCalculator/Advantages.js";
 import Footer from "../sections/Footer.tsx";
+import Header from "../sections/Header.tsx";
 const FormCalculator = () => {
     return (
         <div>
+            <Header/>
             <Advantages/>
             <Calculator/>
             <Footer/>
