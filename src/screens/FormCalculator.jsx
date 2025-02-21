cbf2d69bf86720b8a97eb30c5a43c517b29aec20
Ori <ori@stallae.com>
2025-02-21 11:26:24 -0300

    feat: add Footer

--- src/screens/FormCalculator.jsx
+++ src/screens/FormCalculator.jsx
@@ -1,11 +1,12 @@
 import Calculator from "../sections/FormCalculator/Calculator.jsx";
 import Advantages from "../sections/FormCalculator/Advantages.js";
-
+import Footer from "../sections/Footer.tsx";
 const FormCalculator = () => {
     return (
         <div>
             <Advantages/>
             <Calculator/>
+            <Footer/>
         </div>
     );
 }
