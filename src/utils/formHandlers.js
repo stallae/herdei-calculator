e71511235feac63e0761afea7130a12088965760
Ori <ori@stallae.com>
2025-02-24 12:10:15 -0300

    cleanup values

--- src/utils/formHandlers.js
+++ src/utils/formHandlers.js
@@ -1,17 +1,1 @@
-export const handleClear = ({
-  setMaritalStatus,
-  setStateOfResidence,
-  setNumberOfGoods,
-  setGoods,
-  setName,
-  setEmail,
-  setPhone
-}) => {
-  setMaritalStatus('')
-  setStateOfResidence('')
-  setNumberOfGoods('')
-  setGoods([])
-  setName('')
-  setEmail('')
-  setPhone('')
-}
+// todo submition form values
