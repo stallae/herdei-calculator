bb7c7693668769c3244b81f367f4a31d9c4bf5ba
Ori <ori@stallae.com>
2025-02-27 11:28:37 -0300

    feat: add height event listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FormCalculator from "./screens/FormCalculator";
-import OutputCalculator from "./screens/OutputCalculator";
-import OnlyCalculator from "./screens/OnlyCalculator";
+import { useEffect } from "react";
+import Calculator from "../sections/FormCalculator/Calculator.jsx";
+import Footer from "../sections/Footer.tsx";
+import Menu from "../sections/Menu.tsx";
+import Header from "../sections/Header.tsx";
+import Steps from "../sections/FormCalculator/Steps.tsx";
 
-function App() {
+const FormCalculator = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = `
+      function sendHeight() {
+        const height = document.documentElement.scrollHeight;
+        window.parent.postMessage({ iframeHeight: height }, "*");
+      }
+
+      window.addEventListener("load", sendHeight);
+      window.addEventListener("resize", sendHeight);
+      setInterval(sendHeight, 500);
+    `;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FormCalculator />} />
-        <Route path="/output" element={<OutputCalculator />} />
-        <Route path="/calculadora" element={<OnlyCalculator />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Menu />
+      <Header />
+      <Steps />
+      <Calculator />
+      <Footer />
+    </div>
   );
-}
+};
 
-export default App;
+export default FormCalculator;
